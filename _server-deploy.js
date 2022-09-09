54491040616631f2b073c1620ff9ae35eb3eb19d
HarunPHero <harun7309@gmail.com>
2022-09-09 12:11:52 +0600

    back-end server

---
+++ _server-deploy.js
@@ -0,0 +1,14 @@
+/**
+ * --------------------------
+ * ONE TIME for your computer
+ * -------------------------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * ----------------
+ *  FOR EACH PROJECT ONE TIME
+ * ----------------------------
+ * 1. heroku create
+ * 2. make sure about git push
+ */
